0cd2b3954460865cc5f7794cb249cefc1c7d6fcf
Alexandre Cochet <alexcochet1@hotmail.fr>
2019-03-26 13:51:24 +0100

    Quête Angular__02 - Le Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { UserProfilesComponent } from './user-profiles/user-profiles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    SignUpComponent,
+    UserProfilesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-profiles/user-profiles.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Button } from 'protractor';
+
+@Component({
+  selector: 'app-user-profiles',
+  templateUrl: './user-profiles.component.html',
+  styleUrls: ['./user-profiles.component.css']
+})
+export class UserProfilesComponent implements OnInit {
+  
+    name= 'Doe'
+    firstName= 'John'
+    age= 25
+    quote= ''
+    photo= 'https://randomuser.me/api/portraits/lego/2.jpg'
+  
+  constructor() { }
+ 
+  ngOnInit() {
+  }
+  
+hide(){
+  const Age1 = document.getElementById('Age');
+  const btn = document.getElementById('ID')
+  btn.addEventListener('click', () => {
+    Age1.style.display = 'none' ;
+});
+}
+
+}
