a3cee2b5db7288897dc061165e03aaf62feccac7
Alexandre Cochet <alexcochet1@hotmail.fr>
2019-04-16 17:28:10 +0200

    Atelier_Angular_06

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -4,13 +4,22 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
+
 })
+
+
 export class SignUpComponent implements OnInit {
+ 
   email = 'email';
-  songList:string[]=["Alejandro", "Le temps est bon"]
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  onSubmit() {
+
+    console.log('form submitted');
   }
 
 }
